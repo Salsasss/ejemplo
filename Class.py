aeed27287823696321343dbbd9592543c52e91e0
Salsasss <aaronsalasn@gmail.com>
2025-06-02 17:00:01 -0600

    se añadio un metodo

--- Class.py
+++ Class.py
@@ -8,3 +8,9 @@ class Class:
 
     def __str__(self):
         return f"{self.name}: {self.description if self.description else 'No description provided'}"
+
+
+    def __eq__(self, other):    
+        if not isinstance(other, Class):
+            return NotImplemented
+        return self.name == other.name and self.description == other.description
